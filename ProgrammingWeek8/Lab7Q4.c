5e5d5d5a1db146165a4ab972ca9c85e6786596c5
nady-lab <125826809+nady-lab@users.noreply.github.com>
2025-11-21 18:48:04 +0000

    new file:   ProgrammingWeek8/Lab7Q4 	modified:   ProgrammingWeek8/Lab7Q4.c

--- ProgrammingWeek8/Lab7Q4.c
+++ ProgrammingWeek8/Lab7Q4.c
@@ -12,24 +12,43 @@
 #define ARRAY_SIZE 4
 
 int main() {
-    int nums1[ARRAY_SIZE] = {0};
-    int nums2[ARRAY_SIZE] = {0};
-    int i = 0;
+    int nums1[ARRAY_SIZE];
+    int nums2[ARRAY_SIZE];
+    int i;
 
     for (i = 0; i < ARRAY_SIZE; i++)
     {
         printf("Enter a int: ");
         scanf("%d", &nums1[i]);
+
     }// end for 
 
-    for (i = 0; i < ARRAY_SIZE; i++)
+    printf("Array entered:\n");
+    for(i = 0; i < ARRAY_SIZE; i++)
     {
         printf("%d\n", nums1[i]);
-    }// end for 
+    }
 
-    for (i = 0; i < ARRAY_SIZE; i++)
+    /*
+    nums2[1] = nums1[0];
+    nums2[0] = nums1[1];
+    nums2[3] = nums1[2];
+    nums2[2] = nums1[3];
+    */
+
+    
+    //Swapping each pair in a loop
+    for (i = 0; i < ARRAY_SIZE; i += 2) 
     {
-        
+        nums2[i] = nums1[i + 1]; // swap first of the pair
+        nums2[i + 1] = nums1[i]; // swap second of the pair
+    }
+    
+
+    printf("New array: \n");
+    for(i = 0; i < ARRAY_SIZE; i++)
+    {
+        printf("%d\n", nums2[i]);
     }
 
     return 0;
